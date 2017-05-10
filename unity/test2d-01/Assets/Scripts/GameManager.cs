887726f8f459263f9a93e3f2b8ae3ea115c4546e
heri3x <heri3x@himehagi.com>
2017-05-11 08:57:59 +0900

    Add Enemies: Sam and Betty

---
+++ unity/test2d-01/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public EnemyController Enemy;
+
+    public EnemyData[] LevelData;
+
+    [Range(1, 5)]
+    public int Level;
+
+    void Start()
+    {
+        //level <= arrya size
+        if (Level > LevelData.Length)
+        {
+            Level = LevelData.Length;
+        }
+
+        //pass SO info into our enemy
+        Enemy.Data = LevelData[Level - 1];
+    }
+
+}
