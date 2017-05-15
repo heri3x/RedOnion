64f5dc5cf1467e95f6960153cbd9c4e2cc60b8e4
heri3x <heri3x@himehagi.com>
2017-05-15 13:44:13 +0900

    ポストエフェクトのテストコード追加 ・セピア調エフェクト（シーン Main > Main Camera のインスペクタで Post Effect を有効にすると機能する）

---
+++ unity/test2d-01/Assets/Scripts/PostEffects/PostEffect.cs
@@ -0,0 +1,17 @@
+﻿//
+//【Unityシェーダ入門】画面をセピア色にするポストエフェクトを作る
+//http://nn-hokuson.hatenablog.com/entry/2017/05/09/202141
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour
+{
+    public Material PostEffectMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, PostEffectMaterial);
+    }
+}
