efe838bcb4e6812bd955e9c6cec2bf408874bedd
heri3x <heri3x@himehagi.com>
2017-05-15 18:32:27 +0900

    Attributeのテストコード追加

--- unity/test2d-01/Assets/Scripts/Scene Controllers/MainSceneController.cs
+++ unity/test2d-01/Assets/Scripts/Scene Controllers/MainSceneController.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[AddComponentMenu("Test/MainSceneController")]
 public class MainSceneController : MonoBehaviour
 {
+    public int TestValue1;
+
+    [Space(30)]
+
+    public int TestValue2;
+
+
+
+    [ContextMenu("MainScene Do Something")]
+    void DoSomething()
+    {
+        Debug.Log("Perform operation");
+    }
+
+
     private void Awake()
     {
         // もしシングルトンインスタンスがなければ生成
